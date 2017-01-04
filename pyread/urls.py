e4274d7565818d80173fbc7d41d8fda96c8ed74e
mihilbabin <iamsuperman@ukr.net>
2017-01-04 21:03:48 +0200

    Added first urls and templates

--- pyread/urls.py
+++ pyread/urls.py
@@ -1,12 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from pages import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^read/', include('pages.urls')),
+    url(r'^$', views.index, name='root'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
